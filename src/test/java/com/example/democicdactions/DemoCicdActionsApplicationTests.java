153a5ce29df2d367b261062e4650dab3e6b83c63
DimuthuWijegunarathn <d.wijegunarathna@aecsolutions.com>
2023-10-09 12:09:39 +0200

    first commit

---
+++ src/test/java/com/example/democicdactions/DemoCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democicdactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
